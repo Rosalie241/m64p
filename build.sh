1982a8e80c3516c77ab80dff4607b22940e09307
Tim Wanders <tim241@mailbox.org>
2019-06-15 11:07:00 +0200

    update

---
+++ build.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+UNAME=$(uname -s)
+if [[ $UNAME == *"MINGW"* ]]; then
+  suffix=".dll"
+  if [[ $UNAME == *"MINGW64"* ]]; then
+    mingw_prefix="mingw64"
+  else
+    mingw_prefix="mingw32"
+  fi
+else
+  suffix=".so"
+fi
+
+
+install_dir="$PWD/mupen64plus"
+mkdir -p "$install_dir"
+base_dir="$PWD"
+
+cd "$base_dir"/mupen64plus-core/projects/unix
+make -j$(nproc) all
+cp -P "$base_dir"/mupen64plus-core/projects/unix/*$suffix* "$install_dir"
+cp "$base_dir"/mupen64plus-core/data/* $install_dir
+
+cd "$base_dir"/mupen64plus-rsp-hle/projects/unix
+make -j$(nproc) all
+cp "$base_dir"/mupen64plus-rsp-hle/projects/unix/*$suffix "$install_dir"
+
+cd "$base_dir"/mupen64plus-input-sdl/projects/unix
+make -j$(nproc) all
+cp "$base_dir"/mupen64plus-input-sdl/projects/unix/*$suffix "$install_dir"
+cp "$base_dir"/mupen64plus-input-sdl/data/* $install_dir
+
+cd "$base_dir"/mupen64plus-audio-sdl2/projects/unix
+make -j$(nproc) all
+cp "$base_dir"/mupen64plus-audio-sdl2/projects/unix/*$suffix "$install_dir"
+
+mkdir -p "$base_dir"/mupen64plus-gui/build
+cd "$base_dir"/mupen64plus-gui/build
+
+if [[ $UNAME == *"MINGW"* ]]; then
+  qmake ../mupen64plus-gui.pro
+  make -j$(nproc) release
+  cp "$base_dir"/mupen64plus-gui/build/release/mupen64plus-gui.exe "$install_dir"
+else
+  qmake ../mupen64plus-gui.pro
+  make -j$(nproc)
+  cp "$base_dir"/mupen64plus-gui/build/mupen64plus-gui "$install_dir"
+fi
+
+cd $base_dir/GLideN64/src
+./getRevision.sh
+
+mkdir -p "$base_dir"/GLideN64/src/GLideNUI/build
+cd "$base_dir"/GLideN64/src/GLideNUI/build
+if [[ $UNAME == *"MINGW"* ]]; then
+  qmake ../GLideNUI.pro
+  make -j$(nproc) release
+else
+  qmake ../GLideNUI.pro
+  make -j$(nproc)
+fi
+
+cd "$base_dir"/GLideN64/projects/cmake
+sed -i 's/GLideNUI\/build\/debug\/libGLideNUI.a/GLideNUI\/build\/release\/libGLideNUI.a/g' ../../src/CMakeLists.txt
+if [[ $UNAME == *"MINGW"* ]]; then
+  # Fix compiling error
+  sed -i "s|C:\/building\/msys32|C:/msys64|g" /$mingw_prefix/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake
+  sed -i 's/check_ipo_supported(RESULT result)//g' ../../src/CMakeLists.txt
+  cmake -G "MSYS Makefiles" -DVEC4_OPT=On -DCRC_OPT=On -DMUPENPLUSAPI=On ../../src/
+else
+  rm -rf ../../src/GLideNHQ/inc
+  cmake -DUSE_SYSTEM_LIBS=On -DVEC4_OPT=On -DCRC_OPT=On -DMUPENPLUSAPI=On ../../src/
+fi
+
+make -j4
+
+if [[ $UNAME == *"MINGW"* ]]; then
+  cp mupen64plus-video-GLideN64$suffix "$install_dir"
+else
+  cp plugin/Release/mupen64plus-video-GLideN64$suffix "$install_dir"
+fi
+cp "$base_dir"/GLideN64/ini/GLideN64.custom.ini "$install_dir"
+
+cd "$base_dir"
+
+strip "$install_dir"/*$suffix
+
+if [[ $UNAME == *"MINGW"* ]]; then
+  if [[ $UNAME == *"MINGW64"* ]]; then
+    my_os=win64
+  else
+    my_os=win32
+  fi
+  
+  bash ../bundle_dlls.sh "$install_dir/" "$install_dir/mupen64plus-gui.exe" "/$mingw_prefix/bin/"  
+  windeployqt.exe "$install_dir/"
+  
+  cp $base_dir/7za.exe "$install_dir"
+else
+  if [[ $HOST_CPU == "i686" ]]; then
+    my_os=linux32
+  else
+    my_os=linux64
+  fi
+fi
+
+rm -rf "$base_dir"/*.zip
+
+zip -r mupen64plus-GLideN64-$my_os.zip mupen64plus
---
+++ bundle_dlls.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#
+# ./bundle_dlls.sh "./bin" "./test,exe" "/mingw53/bin"
+#
+BINDIR="$1"
+EXE="$2"
+
+paths=("$3")
+
+function findAndCopyDLL() {
+    for i in "${paths[@]}"
+    do
+        FILE="$i/$1"
+        if [ -f $FILE ] && [ ! -f "$BINDIR/$1" ]; then
+           cp $FILE $BINDIR
+           echo "Found $1 in $i"
+           copyForOBJ $FILE
+           return 0
+        fi
+    done
+
+    return 1
+}
+
+function copyForOBJ() {
+    dlls=`objdump.exe -p $1 | grep 'DLL Name:' | sed -e "s/\t*DLL Name: //g"`
+    while read -r filename; do
+        findAndCopyDLL $filename
+    done <<< "$dlls"
+}
+
+copyForOBJ $EXE
